3e369cb215c49d56e501567ea1c6fe836bb914eb
Luiz Paulo <lindennerd@gmail.com>
2015-10-24 22:57:16 -0200

    added pages; application reorganized

--- library/lib/route/initial_routes/routes.js
+++ library/lib/route/initial_routes/routes.js
@@ -1,3 +1,27 @@
-Router.route('/', function () {
+Router.route('/', {name: 'home'}, function () {
   this.render('home');
+});
+
+Router.route('/search',{name: 'search'} ,function () {
+  this.render('search');
+});
+
+Router.route('/hottest',{name: 'hottest'}, function () {
+  this.render('hottest');
+});
+
+Router.route('/news', {name: 'news'},function () {
+  this.render('news');
+});
+
+Router.route('/toprated', {name: 'toprated'}, function () {
+  this.render('toprated');
+});
+
+Router.route('/tags',{name: 'tags'}, function () {
+  this.render('tags');
+});
+
+Router.route('/about', {name: 'about'},function () {
+  this.render('about');
 });
